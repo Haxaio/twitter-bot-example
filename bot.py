56f984559840a98cc8abc66ac46356f5d5fbfad1
M Harding <m@haxa.io>
2022-07-06 17:26:08 -0400

    Update env vars

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import tweepy
+import os
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler("CONSUMER_KEY", os.getenv("TWITTER_CONSUMER_KEY"))
+auth.set_access_token("ACCESS_TOKEN", os.getenv("TWITTER_ACCESS_TOKEN"))
 
 # Create API object
 api = tweepy.API(auth)
